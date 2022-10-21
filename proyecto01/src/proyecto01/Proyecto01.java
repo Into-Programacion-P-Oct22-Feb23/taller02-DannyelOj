daabbfbb30c4591b99d674d084b25fb3d818f1a7
Daniel Ojeda <dmojeda@utpl.edu.ec>
2022-10-21 11:55:04 -0500

    tallaer numero dos

---
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto01;
+
+/**
+ *
+ * @author UTPL
+ */
+public class Proyecto01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     system.out.println("Ejemplo básico de Java");
+     system.out.println("Versionado a través de Git"); 
+     system.out.println("Manejado desde GitHub");   
+    }
+    
+}
